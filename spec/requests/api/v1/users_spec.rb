abf07f4c0fd26128f7980edf0595c15bcee6607c
andre-tlima <andre-tlima@hotmail.com>
2019-03-19 17:08:00 +0000

    Criação de testes e autenticação

---
+++ spec/requests/api/v1/users_spec.rb
@@ -0,0 +1,73 @@
+require 'rails_helper'
+
+RSpec.describe 'Users API', type: :request do
+    #Cria o Usuário
+    let!(:user) {create(:user)}
+    let(:user_id) {user.id}
+    
+    #Define Host
+    before {host! "localhost:3000/api"} 
+    
+    describe "GET user/:id" do #Get = Consultar
+        
+        before do #define variavel headers ANTES DE COMEÇAR
+           headers = {"Accept" => "application/vnd.projetofase8.v1"} 
+            get "/users/#{user_id}", params: {}, headers: headers
+        end
+        
+        #Quando o Usuário existir, retorna um Usuário como JSON e retorna o Código 200(deu certo)
+        context "when the user exists" do
+            it "returns the user" do
+                user_response = JSON.parse(response.body)
+                expect(user_response["id"]).to eq(user_id)
+            end
+            #Retorna o codigo 200
+            it "return status code 200" do
+                expect(response).to have_http_status(200)
+            end
+        end
+        #Quando o usuário não existir, retorna erro 404(requisição não encontrada)
+        context "when the user does not exists" do
+            #Esse método let é um teste que não temos, para provar
+            let(:user_id){ 10000 }
+            
+            it "return status code 404" do
+                expect(response).to have_http_status(404)
+            end
+        end
+    end
+    describe "POST user/" do
+        before do #define variavel headers ANTES DE COMEÇAR
+           headers = {"Accept" => "application/vnd.projetofase8.v1"} 
+            post "/users/", params: { user: user_params }, headers: headers 
+            #Headers tá passando os parametros de usuário AINDA NÃO CONFIGURAMOS ELES
+        end
+        #Quando as requisições são validas, retorna o Código 201
+        context "when the request params are valid" do
+            let(:user_params){ attributes_for(:user) }
+            #retorna o código 201
+            it "returns status code 201" do
+                expect(response).to have_http_status(201)
+            end
+            
+            it "returns json data for the created user" do
+                # Retorna o Usuário criado em Json
+                user_response = JSON.parse(response.body)
+                expect(user_response["email"]).to eq(user_params[:email])
+            end
+        end
+        # Quando os parametros da requisição for invalida, retorna o código 422(Requisição está certa mas voce passou a requisição errado)
+        context "when the request params are invalid" do
+            #Esse método let é um teste que não temos, para provar
+            let(:user_params){attributes_for(:user, email: "email_invalido@") }
+            it "return status code 422" do
+                expect(response).to have_http_status(422)
+            end
+            # Retorna uma chave com um Json com o erro da requisição
+            it "returns json data for the errors" do
+                user_response = JSON.parse(response.body)
+                expect(user_response).to have_key('errors')
+            end
+        end
+    end
+end
