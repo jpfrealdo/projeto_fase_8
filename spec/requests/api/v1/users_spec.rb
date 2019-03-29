c09ed0955e7c1489261d829e3619abed7f01d1de
andre-tlima <andre-tlima@hotmail.com>
2019-03-29 12:56:20 +0000

    Criando PUT, DELETE e Refatorando

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -4,22 +4,22 @@ RSpec.describe 'Users API', type: :request do
     #Cria o Usuário
     let!(:user) {create(:user)}
     let(:user_id) {user.id}
-    
+    let(:headers) {{"Accept" => "application/vnd.projetofase8.v1"}} 
     #Define Host
     before {host! "localhost:3000/api"} 
     
     describe "GET user/:id" do #Get = Consultar
         
         before do #define variavel headers ANTES DE COMEÇAR
-           headers = {"Accept" => "application/vnd.projetofase8.v1"} 
+          
             get "/users/#{user_id}", params: {}, headers: headers
         end
         
         #Quando o Usuário existir, retorna um Usuário como JSON e retorna o Código 200(deu certo)
         context "when the user exists" do
             it "returns the user" do
-                user_response = JSON.parse(response.body)
-                expect(user_response["id"]).to eq(user_id)
+               
+                expect(json_body["id"]).to eq(user_id)
             end
             #Retorna o codigo 200
             it "return status code 200" do
@@ -38,7 +38,7 @@ RSpec.describe 'Users API', type: :request do
     end
     describe "POST user/" do
         before do #define variavel headers ANTES DE COMEÇAR
-           headers = {"Accept" => "application/vnd.projetofase8.v1"} 
+          
             post "/users/", params: { user: user_params }, headers: headers 
             #Headers tá passando os parametros de usuário AINDA NÃO CONFIGURAMOS ELES
         end
@@ -52,8 +52,8 @@ RSpec.describe 'Users API', type: :request do
             
             it "returns json data for the created user" do
                 # Retorna o Usuário criado em Json
-                user_response = JSON.parse(response.body)
-                expect(user_response["email"]).to eq(user_params[:email])
+               
+                expect(json_body["email"]).to eq(user_params[:email])
             end
         end
         # Quando os parametros da requisição for invalida, retorna o código 422(Requisição está certa mas voce passou a requisição errado)
@@ -65,9 +65,44 @@ RSpec.describe 'Users API', type: :request do
             end
             # Retorna uma chave com um Json com o erro da requisição
             it "returns json data for the errors" do
-                user_response = JSON.parse(response.body)
-                expect(user_response).to have_key('errors')
+             
+                expect(json_body).to have_key('errors')
             end
+        end
+    end
+    describe "PUT user/:id" do
+        before do
+            put "/users/#{user_id}", params: { user: user_params }, headers: headers
+        end
+        context "when the request params are valid" do
+            let(:user_params){{ email: 'novo@email.com'}}
+            it "returns status code 200" do
+                expect(response).to have_http_status(200)
+            end
+            it "returns json data for the created user" do
+                expect(json_body["email"]).to eq(user_params[:email])
+            end
+        end
+        context "when the resquest params are invalid"do
+            let(:user_params){{email: "email_invalido@"}}
+            it "return status code 422" do
+                expect(response).to have_http_status(422)
+            end
+            it "returns json data for the errors" do
+                expect(json_body).to have_key('errors')
+            end
+        end
+    end
+    describe "DELETE user/:id" do
+        before do
+            delete "/users/#{user_id}", params: {}, headers: headers
+        end
+        it"returns status code 204" do
+            expect(response).to have_http_status(204)
+        end
+        
+        it"removes the user from database" do
+            expect(User.find_by(id: user.id) ).to be_nil
         end
     end
 end
